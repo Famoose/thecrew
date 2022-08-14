ec921669adb38d084a26d99149f12b778209f014
lukas nydegger <lukas.nydegger@students.ffhs.ch>
2022-08-14 02:49:57 +0200

    add socketio

--- client/src/app.ts
+++ client/src/app.ts
@@ -3,3 +3,30 @@ import { Search } from './components/Search'
 console.log('file loaded')
 
 window.customElements.define('search-result', Search)
+
+import { io, Socket } from 'socket.io-client'
+import { ClientToServerEvents, ServerToClientEvents } from './types'
+
+// please note that the types are reversed
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('', {
+    path: '/api',
+})
+
+socket.on('connect', () => {
+    console.log(socket.id)
+})
+
+socket.emit('hello')
+socket.on('noArg', () => {
+    console.log(`noArg`)
+})
+
+socket.on('basicEmit', (a, b, c) => {
+    // a is inferred as number, b as string and c as buffer
+    console.log(`${a}, ${b}, ${c}`)
+})
+
+socket.on('withAck', (d, _callback) => {
+    console.log(`${d}`)
+    // d is inferred as string and callback as a function that takes a number as argument
+})
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,37 @@
-console.log('hallo welt')
-console.log('test')
+import { Server } from 'socket.io'
+import http from 'http'
+import {
+    ClientToServerEvents,
+    InterServerEvents,
+    ServerToClientEvents,
+    SocketData,
+} from './types'
 
-export type test = {
-    name: string
-    age: number
-}
+const httpServer = http.createServer()
+
+console.log('hallo welt')
+
+const io = new Server<
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
+>(httpServer)
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+    socket.emit('noArg')
+    socket.emit('basicEmit', 1, '2', Buffer.from([3]))
+    socket.emit('withAck', '4', (_e) => {
+        // e is inferred as number
+        console.log(`${_e} ack`)
+    })
+
+    // works when broadcast to all
+    io.emit('noArg')
+
+    // works when broadcasting to a room
+    io.to('room1').emit('basicEmit', 1, '2', Buffer.from([3]))
+})
+
+httpServer.listen(8080)
