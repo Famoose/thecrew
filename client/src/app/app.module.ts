3e31b6cf836aa94e27b7d69fc5d7a37a33339ac5
Damaris <damaris.zosso@students.ffhs.ch>
2022-09-14 20:03:24 +0200

    setup pages home and lobbieslist

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { ChatSocket, LobbySocket, MainSocket } from './config/socket.config'
 import { SocketIoModule } from 'ngx-socket-io'
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
+import { LobbieslistComponent } from './pages/lobbieslist/lobbieslist.component'
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, HomeComponent, LobbieslistComponent],
     imports: [BrowserModule, AppRoutingModule, SocketIoModule, FormsModule],
     providers: [MainSocket, LobbySocket, ChatSocket],
     bootstrap: [AppComponent],
