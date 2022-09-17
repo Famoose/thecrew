4d9a00a78fe94e180219e0ff64499b54cafc4127
Damaris <damaris.zosso@students.ffhs.ch>
2022-09-17 15:10:35 +0200

    first try routing home to lobbieslist

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LobbieslistComponent} from "./pages/lobbieslist/lobbieslist.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: './pages/lobbieslist', component: LobbieslistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
